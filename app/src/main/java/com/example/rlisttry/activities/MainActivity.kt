feb28ccc7b43e85d6d2a5fe14d09b82d57fc1495
zuevasasha78 <zuevasasha78@gmail.com>
2020-02-27 22:20:26 +0300

    change MainActivity for large screen

--- app/src/main/java/com/example/rlisttry/activities/MainActivity.kt
+++ app/src/main/java/com/example/rlisttry/activities/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.FrameLayout
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rlisttry.ListDetailFragment
 import com.example.rlisttry.ListSelectionFragment
 import com.example.rlisttry.R
 import com.example.rlisttry.TaskList
@@ -15,9 +16,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(),
     ListSelectionFragment.OnListItemFragmentInteractionListener {
 
-
+    private var largeScreen = false
+    private var listFragment: ListDetailFragment? = null
     private var fragmentContainer: FrameLayout? = null
-    private val listSelectionFragment: ListSelectionFragment =
+    private var listSelectionFragment: ListSelectionFragment =
         ListSelectionFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +27,12 @@ class MainActivity : AppCompatActivity(),
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        listSelectionFragment =
+            supportFragmentManager.findFragmentById(R.id.list_selection_fragment) as
+                    ListSelectionFragment
         fragmentContainer = findViewById(R.id.fragment_container)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, listSelectionFragment)
-            .commit()
+        largeScreen = fragmentContainer != null
 
         fab.setOnClickListener {
             showCreateListDiolog()
@@ -48,6 +51,26 @@ class MainActivity : AppCompatActivity(),
 
     override fun onListItemClick(list: TaskList) {
         showListDetail(list)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        title = resources.getString(R.string.app_name)
+        listFragment?.lists?.let {
+            listSelectionFragment.listDataManager.saveList(it)
+        }
+        listFragment?.let {
+            supportFragmentManager
+                .beginTransaction()
+                .remove(it)
+                .commit()
+            listFragment = null
+        }
+
+        fab.setOnClickListener {
+            showCreateListDiolog()
+        }
     }
 
     fun showCreateListDiolog() {
@@ -71,9 +94,42 @@ class MainActivity : AppCompatActivity(),
     }
 
     fun showListDetail(list: TaskList) {
-        val listDetailIntent = Intent(this, ListDetailActivity::class.java)
-        listDetailIntent.putExtra(INTENT_LIST_KEY, list)
-        startActivityForResult(listDetailIntent, LIST_DETAIL_REQUEST_CODE)
+        if (!largeScreen) {
+            val listDetailIntent = Intent(this, ListDetailActivity::class.java)
+            listDetailIntent.putExtra(INTENT_LIST_KEY, list)
+
+            startActivityForResult(listDetailIntent, LIST_DETAIL_REQUEST_CODE)
+        } else {
+            title = list.name
+
+            listFragment = ListDetailFragment.newInstence(list)
+            listFragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, it, getString(R.string.list_fragment_tag))
+                    .addToBackStack(null)
+                    .commit()
+            }
+
+            fab.setOnClickListener {
+                showCreateTaskDialog()
+            }
+        }
+    }
+
+    fun showCreateTaskDialog() {
+        val taskEditText = EditText(this)
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText)
+            .setPositiveButton(R.string.add_task) { dialog, _ ->
+                val task = taskEditText.text.toString()
+                listFragment?.addTask(task)
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 
     companion object {
